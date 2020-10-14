13ce98fa48e106e69aa967a3f11888618fa09c8c
NanRos <nancyrosa956@gmail.com>
2020-10-14 00:49:58 -0700

    Add functionality to compose tweet,post to twitter, manually insert into Timeline.

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -7,8 +8,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.icu.util.LocaleData;
 import android.os.Bundle;
+import android.text.ParcelableSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +23,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +33,8 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimeLineActivity";
+    // REQUEST_CODE can be any value we like, used to determine the result type later
+    private final int REQUEST_CODE = 20;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -84,22 +90,41 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.addOnScrollListener(scrollListener);
         populateHomeTimeLine();
     }
+    //hamburger except with icon
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate the menu; this adds to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+    // when menu item clicked
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() ==R.id.compose){
             //compose icon tapped
-            Toast.makeText(this, "compose clicked", Toast.LENGTH_SHORT).show();
             //navigate to compose activity
+            Intent intent = new Intent(this,ComposeActivity.class);
+            startActivityForResult(intent,REQUEST_CODE);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    // result returned from Compose Activity to parent
+    //result code defined by android
+    //data defined by child activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode ==REQUEST_CODE && resultCode == RESULT_OK){
+            // GET THE DATA FROM THE INTENT (the tweet)
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // Update the RV WITH THE TWEET
+            //Modify data source of tweets
+            tweets.add(0,tweet);
+            // update the adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0); //go to where we placed tweet (top)
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     // this is where we will make another API call to get the next page of tweets and add the objects to our current list of tweets
