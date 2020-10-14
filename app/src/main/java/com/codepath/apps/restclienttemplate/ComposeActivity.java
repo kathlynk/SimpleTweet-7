f6aedc7187c0e7957c4f8a4cbe13857a99b4f4e0
NanRos <nancyrosa956@gmail.com>
2020-10-14 00:52:48 -0700

    Add activity to create tweet,Menu UI

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,91 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.LoginFilter;
+import android.text.ParcelableSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    public static final int MAX_TWEET_LENGTH = 140;
+    public static final String TAG = "ComposeActivty";
+    EditText etCompose;
+    Button btnTweet;
+
+    TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getRestClient(this);
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+
+        //Set a click listener on the button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //validate length of tweet
+                String tweetContent = etCompose.getText().toString();
+                //empty
+                if(tweetContent.isEmpty()){
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet can not be empty!",
+                           Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // too long
+                if(tweetContent.length() > MAX_TWEET_LENGTH){
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // make an API call to twitter to publish the tweet
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "OnSuccess to publish Tweet!");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet says:"+ tweet.body);
+                            // Prepare data intent
+                            Intent intent = new Intent();
+                            // Pass relevant data back as a result
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            // Activity finished ok, return the data
+                            setResult(RESULT_OK, intent); // set result code and bundle data for
+                            // response
+                            finish(); // closes the activity, pass data to parent
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Json Exception",e);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG,"onFailure to publish tweet",throwable);
+                    }
+                });
+
+            }
+        });
+
+    }
+}
